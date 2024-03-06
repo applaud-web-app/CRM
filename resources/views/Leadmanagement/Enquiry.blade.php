e654cd0dc6a3f83955202426e5b9536eb3f31c6e
Vinay Singh <vinaysinghworkspace@gmail.com>
2024-03-06 09:34:23 +0530

    Changed table structure

--- resources/views/Leadmanagement/Enquiry.blade.php
+++ resources/views/Leadmanagement/Enquiry.blade.php
@@ -39,9 +39,9 @@
                                     <tr>
                                         <th>#</th>
                                         <th>Enquiry Name</th>
-                                        <th>Interest In</th>
-                                        <th>Email Address</th>
                                         <th>Mobile No.</th>
+                                        <th>Email Address</th>
+                                        <th>Interest In</th>
                                         <th>Date</th>
                                         <th>Sources</th>
                                         <th width="100px">Action</th>
@@ -279,10 +279,10 @@
 
 <script type="text/javascript">
     $(function () {
-        
       var table = $('.data-table').DataTable({
           processing: true,
-          serverSide: true, 
+          serverSide: true,
+          searching: false, 
           ajax: "{{ route('enquiry') }}",
           columns: [
               {data: 'id', name: 'id'},
@@ -298,81 +298,6 @@
       $('#search').on('keyup', function() {
         table.search(this.value).draw();
     });
-        
     });
   </script>
 @endpush
-
-
-{{-- 
-<table class="data-table table display text-black" id="example5">
-    <thead>
-        <tr>
-            <th>
-                #
-            </th>
-
-            <th>Enquiry Name</th>
-            <th>Interest In</th>
-            <th>Email Address</th>
-            <th>Mobile No.</th>
-            <th>Date</th>
-            <th>Sources</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody> --}}
-{{-- @php $i=1; @endphp
-        @foreach ($enquiries as $enquiry)
-        <tr>
-            <td>{{$i}}</td>
-            <td>{{$enquiry->name}}</td>
-            <td>{{$enquiry->interested}}</td>
-            <td>{{$enquiry->email}}</td>
-            <td>{{$enquiry->mobile}}</td>
-            <td class="wspace-no">{{$enquiry->created_at}}</span></td>
-            <td class="wspace-no">{{$enquiry->source}} </td>
-
-            <td>
-                <div class="dropdown text-sans-serif">
-                    <button class="btn btn-primary light sharp" type="button"
-                        id="order-dropdown-0" data-bs-toggle="dropdown"
-                        data-boundary="viewport" aria-haspopup="true" aria-expanded="false">
-                        <span>
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                                xmlns:xlink="http://www.w3.org/1999/xlink" width="18px"
-                                height="18px" viewBox="0 0 24 24" version="1.1">
-                                <g stroke="none" stroke-width="1" fill="none"
-                                    fill-rule="evenodd">
-                                    <rect x="0" y="0" width="24" height="24"></rect>
-                                    <circle fill="#000000" cx="5" cy="12"
-                                        r="2"></circle>
-                                    <circle fill="#000000" cx="12" cy="12"
-                                        r="2"></circle>
-                                    <circle fill="#000000" cx="19" cy="12"
-                                        r="2"></circle>
-                                </g>
-                            </svg>
-                        </span>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-end border py-0"
-                        aria-labelledby="order-dropdown-0">
-                        <div class="py-2">
-                            <a class="dropdown-item" href="javascript:void(0);"><i
-                                    class="far fa-eye"></i> View</a>
-                            <a class="dropdown-item" href="javascript:void(0);"><i
-                                    class="far fa-edit"></i> Edit</a>
-                            <a class="dropdown-item" href="javascript:void(0);"><i
-                                    class="fas fa-trash-alt"></i> Delete</a>
-
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item text-secondary"
-                                href="convert-to-lead.php"><i
-                                    class="far fa-check-square"></i> Convert To Lead</a>
-                        </div>
-                    </div>
-            </td>
-        </tr>
-        @endforeach --}}
-{{-- </tbody>
-</table> --}}
